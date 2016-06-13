68148a94eedc0fd7956eb9104217593f553fb362
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2016-06-13 11:00:57 +0200

    deleted binary files (compiler warning: "Named COMMON block 'par6' shall be of the same size as elsewhere (56 vs 24 bytes)")

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,7 +1,14 @@
-file <- if (WINDOWS) "wtaq2_1.exe" else "wtaq2_1"  # working directory is ./src
+file <- if (WINDOWS) {
+  "wtaq2_1.exe"}  else {
+    "wtaq2_1"}
+
+# working directory is ./src
 if (file.exists(file)) {
-  path <- file.path(R_PACKAGE_DIR, "bin", R_ARCH)
-  dir.create(path, recursive=TRUE, showWarnings=FALSE)
-  file.copy(file, path, overwrite=TRUE)
+  dest <- file.path(R_PACKAGE_DIR, "bin", R_ARCH)
+  message("Installing ", file, " to ", dest)
+  dir.create(dest, recursive=TRUE, showWarnings=FALSE)
+  file.copy(file, dest, overwrite=TRUE)
   file.remove(c(file, list.files(pattern = "\\.o$")))
+
 }
+
